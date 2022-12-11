2fe505fb095abbe0aaec1679883aee46f24f8d9c
Caliche Orozco <caliche2da1@gmail.com>
2022-12-10 23:56:35 -0600

    fix: changed the size of cards for sm and md devices

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Card({ id, name, image }) {
-  return <img src={image} alt={name} key={id} />;
+  return <img src={image} alt={name} key={id} className="h-24 w-24 md:h-32 md:w-32 lg:h-max lg:w-max" />;
 }
 
 export default Card;
