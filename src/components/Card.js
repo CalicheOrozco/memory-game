edfcd93cfb47bfe68f8072a60da958ad72ef88bf
Caliche Orozco <caliche2da1@gmail.com>
2023-02-28 17:40:08 -0600

    future: overlay in the cards

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 function Card({ id, name, image }) {
-  return <img src={image} alt={name} key={id} className="h-24 w-24 lg:h-max lg:w-max shadow-md rounded-xl" />;
+  return (
+    <div class="relative">
+    <img src={image} alt={name} key={id} className="h-24 w-24 lg:h-max lg:w-max shadow-md rounded-xl" />
+    <div class="hover:bg-white opacity-50 h-24 w-24 lg:h-max lg:w-max shadow-md rounded-xl absolute top-0 left-0 z-20"></div>
+    </div>
+  )
 }
 
 export default Card;
