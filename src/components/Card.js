e14629bda3e11d4516d0ddb58970fa525ef2d668
Caliche Orozco <caliche2da1@gmail.com>
2023-02-28 18:25:22 -0600

    fix: error with the overlay

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,7 +4,7 @@ function Card({ id, name, image }) {
   return (
     <div class="relative">
     <img src={image} alt={name} key={id} className="h-24 w-24 lg:h-max lg:w-max shadow-md rounded-xl" />
-    <div class="hover:bg-white opacity-50 h-24 w-24 lg:h-max lg:w-max shadow-md rounded-xl absolute top-0 left-0 z-20"></div>
+    <div class="overlay hover:bg-white opacity-50 w-full h-full rounded-2xl absolute top-0 left-0 z-20"></div>
     </div>
   )
 }
